8c499e848d9903af6fb438eb849048cc9d382b39
gureesu <gcambal15@gmail.com>
2024-11-16 17:30:21 +0800

    v0.1

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const navbar = document.querySelector('nav');
+const navLinks = document.querySelector('ul');
+const hamburger = document.querySelector('.hamburger');
+let timeout;
+
+// Function to handle scroll events
+function handleScroll() {
+  const scrollTop = document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.classList.add('hide');
+    navLinks.classList.add('hide');
+  } else {
+    navbar.classList.remove('hide');
+    navLinks.classList.remove('hide');
+  }
+
+  lastScrollTop = Math.max(scrollTop, 0);
+
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    navbar.classList.remove('hide');
+    navLinks.classList.remove('hide');
+  }, 300);
+}
+
+// Function to handle hamburger menu click
+function toggleHamburgerMenu() {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('active');
+  navLinks.classList.toggle('expanded');
+
+  navbar.style.height = navLinks.classList.contains('expanded') ? '295px' : '120px';
+}
+
+// Function to handle window resize events
+function handleResize() {
+  if (window.innerWidth > 700) {
+    navbar.style.height = '120px';
+    navLinks.classList.remove('expanded', 'active');
+    hamburger.classList.remove('active');
+  }
+}
+
+// Event listeners
+window.addEventListener('scroll', handleScroll);
+hamburger.addEventListener('click', toggleHamburgerMenu);
+window.addEventListener('resize', handleResize);
